4c235e96268305d8d40dbb4dca53a4c4598e58f5
dozerl <dozerl@dozerl-desktop>
2009-10-31 16:36:28 -0400

    Created start.asm.  Kenrel loads and hangs.  Added Makefile and linker script.

---
+++ Makefile
@@ -0,0 +1,32 @@
+#
+#Makefile
+
+VERSION 	= 00.01.05
+
+CC 		= gcc
+CFLAGS	=
+ASM		= nasm
+AFLAGS	= -f elf
+LD		= ld
+LFLAGS	= -melf_i386 -T link.ld -Ttext 0x00100000
+
+O_BOOT	= start.o
+OBJ		= $(O_BOOT)
+
+defaults:noclean
+noclean: $(OBJ) dozerl-'$(VERSION)'
+
+
+#/boot files
+start.o:
+	$(ASM) $(AFLAGS) boot/start.asm -o start.o
+
+
+
+#link
+dozerl-'$(VERSION)': $(OBJ)
+	$(LD) $(LFLAGS) -o dozerl-$(VERSION) $(OBJ)
+
+#clean
+clean:
+	rm *.o
---
+++ boot/start.asm
@@ -0,0 +1,39 @@
+;
+;
+;start.asm
+;
+;This is where we setup the information for GRUB
+;the aout kludge and so forth.
+;GRUB enables A20 and flips the protected mode bit 
+;so we are all set with that.
+
+[BITS 32]
+
+jmp start
+
+align 4
+mboot:
+	;Multiboot Macros for GRUB
+	;these are straight from the GRUB web site
+	MULTIBOOT_PAGE_ALIGN		equ 1<<0
+	MULTIBOOT_MEMORY_INFO		equ 1<<1
+	MULTIBOOT_AOUT_KLUDGE		equ 1<<16
+	MULTIBOOT_HEADER_MAGIC		equ 0x1badb002
+	MULTIBOOT_HEADER_FLAGS		equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+	MULTIBOOT_CHECKSUM			equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+	EXTERN code, bss, end
+	
+	dd MULTIBOOT_HEADER_MAGIC
+	dd MULTIBOOT_HEADER_FLAGS
+	dd MULTIBOOT_CHECKSUM
+	
+	;AOUT KLUDGE
+	dd mboot
+	dd code
+	dd bss
+	dd end
+	dd start
+
+[global start]
+start:
+	jmp $	;hang for all time
