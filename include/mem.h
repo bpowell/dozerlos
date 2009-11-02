810bcc0063894ad81b5229e7982213eab20b49b1
dozerl <dozerl@dozerl-desktop>
2009-11-01 21:00:40 -0500

    Added header file for mem.c

---
+++ include/mem.h
@@ -0,0 +1,8 @@
+#ifndef _MEM_H_
+#define _MEM_H_
+
+extern void *memcpy(void *dest, const void *src, size_t count);
+extern void *memset(void *dest, char val, size_t count);
+extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+
+#endif
